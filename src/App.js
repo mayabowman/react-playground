06485f4be6e495c01ca19797872e8a6d1f27dc0d
Maya Bowman <mayashady1@gmail.com>
2019-08-27 16:06:51 -0400

    checkpoint exercises

--- src/App.js
+++ src/App.js
@@ -1,14 +1,35 @@
 import React from 'react';
 import Split from './composition/Split';
 import './App.css';
+import './composition/Tooltip';
+import Tooltip from './composition/Tooltip';
+
+const firstTooltip = (
+  <Tooltip color='hotpink' message='tooltip message'>
+    ipsum
+  </Tooltip>
+)
+
+const secondTooltip = (
+  <Tooltip color='#126bcc' message='another tooltip message'>
+    officiis
+  </Tooltip>
+)
 
 function App() {
   return (
     <main className='App'>
-      <Split />
-      <Split />
-      <Split />
-      <Split />
+      <Split className='left' flexBasis={4}>
+        This is the content of the left split component. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+        <Tooltip message='one more tooltip message'>
+          Necessitatibus?
+        </Tooltip>
+        <Tooltip>{firstTooltip}</Tooltip>
+        <Tooltip>{secondTooltip}</Tooltip>
+      </Split>
+      <Split className='right'>
+        This is the content of the right split component. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Incidunt ex velit suscipit facere officia?
+      </Split>
     </main>
   );
 }
