c985f20431dfab3e9c23068f279e1dedf6b80b14
Maya Bowman <mayashady1@gmail.com>
2019-09-03 08:00:42 -0400

    update to tabs

--- src/state/Tabs.js
+++ src/state/Tabs.js
@@ -2,21 +2,6 @@ import React from 'react';
 
 class Tabs extends React.Component {
   static defaultProps = { tabs: [] };
-  // renderButtons() {
-  //   return this.props.tabs.map((tab, index) => (
-  //     <button key={index}>
-  //       {tab.name}
-  //     </button>
-  //   ))
-  // }
-  // renderContent() {
-  //   const currentTab = this.props.tabs[0]
-  //   return (
-  //     <div className='content'>
-  //       {currentTab.content}
-  //     </div>
-  //   )
-  // }
   render() {
     const buttons = this.props.tabs.map((tab, index) => (
       <button key={index}>
