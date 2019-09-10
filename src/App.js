b3f712c92d0d810dc46cee35f1d1390ed18f58f1
Maya Bowman <mayashady1@gmail.com>
2019-09-10 15:38:01 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import Split from './composition/Split';
 import './App.css';
 // import './composition/Tooltip';
@@ -6,7 +6,9 @@ import './App.css';
 // import TheDate from './state/TheDate';
 // import Counter from './state/Counter';
 // import Tabs from './state/Tabs';
-import Accordion from './state-drills/Accordion';
+// import Accordion from './state-drills/Accordion';
+import DemonymApp from './demonymapp/demonymApp';
+
 
 // const firstTooltip = (
 //   <Tooltip color='hotpink' message='tooltip message'>
@@ -29,30 +31,30 @@ import Accordion from './state-drills/Accordion';
 //     content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
 // ];
 
-const sections = [
-  {
-    title: 'Section 1',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
-  },
-  {
-    title: 'Section 2',
-    content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
-  },
-  {
-    title: 'Section 3',
-    content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
-  },
-]
+// const sections = [
+//   {
+//     title: 'Section 1',
+//     content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+//   },
+//   {
+//     title: 'Section 2',
+//     content: 'Cupiditate tenetur aliquam necessitatibus id distinctio quas nihil ipsam nisi modi!',
+//   },
+//   {
+//     title: 'Section 3',
+//     content: 'Animi amet cumque sint cupiditate officia ab voluptatibus libero optio et?',
+//   },
+// ]
 
-class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <Accordion sections={sections}/>
-      </div>
-    )
-  }
-}
+// class App extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <Accordion sections={sections}/>
+//       </div>
+//     )
+//   }
+// }
 
 // class App extends React.Component {
 //   render() {
@@ -84,5 +86,15 @@ class App extends React.Component {
 //     </main>
 //   );
 // }
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <DemonymApp />
+      </div>
+    )
+  }
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-import HelloWorld from './state-drills/HelloWorld';
-import Bomb from './state-drills/Bomb';
-import RouletteGun from './state-drills/RouletteGun';
-import Tabs from './state/Tabs';
+// import HelloWorld from './state-drills/HelloWorld';
+// import Bomb from './state-drills/Bomb';
+// import RouletteGun from './state-drills/RouletteGun';
+// import Tabs from './state/Tabs';
 
 // ReactDOM.render(<Bomb />, document.getElementById('root'));
 // ReactDOM.render(<HelloWorld />, document.getElementById('root'));
