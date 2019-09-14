d630b3abb12c0c6e1e09f694515db31f4abd3211
Maya Bowman <mayashady1@gmail.com>
2019-09-14 14:05:11 -0400

    initial commit for router exercise

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-// import HelloWorld from './state-drills/HelloWorld';
-// import Bomb from './state-drills/Bomb';
-// import RouletteGun from './state-drills/RouletteGun';
-// import Tabs from './state/Tabs';
+import App from './paltrows-power-toes/App'
 
-// ReactDOM.render(<Bomb />, document.getElementById('root'));
-// ReactDOM.render(<HelloWorld />, document.getElementById('root'));
-// ReactDOM.render(<RouletteGun />, document.getElementById('root'));
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, 
+  document.getElementById('root')
+);
