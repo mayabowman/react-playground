0a6055147aea387d6a8839fc074f0d9d82c69ea4
Maya Bowman <mayashady1@gmail.com>
2019-09-19 10:52:09 -0400

    worked through context exercise

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+// import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './paltrows-power-toes/App'
+// import App from './paltrows-power-toes/App'
+import AppLang from './lang-context/AppLang'
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, 
+  <AppLang />,
   document.getElementById('root')
 );
